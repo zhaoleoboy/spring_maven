eb4e6745b518b5329a63418c30df8e9990e26f49
XIAO1059 <zhaoleoboy@163.com>
2017-07-16 22:53:01 +0800

    add validate code

--- web/src/main/java/com/xiao/controller/HelloController.java
+++ web/src/main/java/com/xiao/controller/HelloController.java
@@ -1,10 +1,15 @@
 package com.xiao.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.xiao.common.RandomValidateCode;
 import com.xiao.entity.User;
 import com.xiao.service.IUserService;
 
@@ -16,14 +21,44 @@ public class HelloController {
 	public @ResponseBody String hello() {
 		return "hello, world";
 	}
-	
+
 	@Autowired
 	private IUserService userService;
-	
-	@RequestMapping(value="/getUser")
-	public @ResponseBody String getUser(){
+
+	@RequestMapping(value = "/getUser")
+	public @ResponseBody String getUser() {
 		User user = userService.listUsers("111");
 		return user.getName();
 	}
-	
+
+	/**
+	 * 跳转到登录页面
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/main")
+	public ModelAndView login() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("login");
+		return mv;
+	}
+
+	// 登录获取验证码
+	@RequestMapping("/getSysManageLoginCode")
+	@ResponseBody
+	public String getSysManageLoginCode(HttpServletResponse response, HttpServletRequest request) {
+		response.setContentType("image/jpeg");// 设置相应类型,告诉浏览器输出的内容为图片
+		response.setHeader("Pragma", "No-cache");// 设置响应头信息，告诉浏览器不要缓存此内容
+		response.setHeader("Cache-Control", "no-cache");
+		response.setHeader("Set-Cookie", "name=value; HttpOnly");// 设置HttpOnly属性,防止Xss攻击
+		response.setDateHeader("Expire", 0);
+		RandomValidateCode randomValidateCode = new RandomValidateCode();
+		try {
+			randomValidateCode.getRandcode(request, response, "imagecode");// 输出图片方法
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+
 }
